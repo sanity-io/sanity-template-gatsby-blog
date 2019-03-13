3f50c6eb91ac80c6568cd3292f36f376387e3efe
Marius Lundgård <studio@mariuslundgard.com>
2019-03-13 10:34:06 +0100

    Change h1 to div

--- template/web/src/components/header.js
+++ template/web/src/components/header.js
@@ -8,9 +8,9 @@ import styles from './header.module.css'
 const Header = ({onHideNav, onShowNav, showNav, siteTitle}) => (
   <div className={styles.root}>
     <div className={styles.wrapper}>
-      <h1 className={styles.branding}>
+      <div className={styles.branding}>
         <Link to='/'>{siteTitle}</Link>
-      </h1>
+      </div>
 
       <button className={styles.toggleNavButton} onClick={showNav ? onHideNav : onShowNav}>
         <Icon symbol='hamburger' />
