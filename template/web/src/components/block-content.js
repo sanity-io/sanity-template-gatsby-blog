283f56bb39c5e00e90b78680e987ad543b608a96
Marius Lundgård <studio@mariuslundgard.com>
2019-03-13 11:12:08 +0100

    Update env files and fix BlockContent props

--- template/web/src/components/block-content.js
+++ template/web/src/components/block-content.js
@@ -1,6 +1,7 @@
+import clientConfig from '../../client-config'
 import BaseBlockContent from '@sanity/block-content-to-react'
 import React from 'react'
 
-const BlockContent = ({blocks}) => <BaseBlockContent blocks={blocks} />
+const BlockContent = ({blocks}) => <BaseBlockContent blocks={blocks} {...clientConfig.sanity} />
 
 export default BlockContent
