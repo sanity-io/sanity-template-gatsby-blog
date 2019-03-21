9c0c289dea52cbc2edab92688e7b8b5d76821364
Marius Lundgård <studio@mariuslundgard.com>
2019-03-21 16:43:33 +0100

    Simplify

--- template/studio/dashboardConfig.js
+++ template/studio/dashboardConfig.js
@@ -1,70 +1,35 @@
-import {NetlifySitePreview} from './plugins/dashboard-widget-project-info-netlify-addons/src'
-
 export default {
   widgets: [
     {name: 'structure-menu'},
     {name: 'welcome'},
     {name: 'sanity-tutorials'},
-    // {name: 'project-info'},
     {
-      name: 'dev/project-info',
+      name: 'project-info',
       options: {
-        disableBuiltinGroups: ['Apps'],
-        items: [
+        __experimental_before: [
           {
-            group: 'Netlify sites',
-            title: 'Studio',
-            value: 'https://test-sanity.netlify.com',
-            component: NetlifySitePreview,
+            name: 'netlify',
             options: {
-              buildHookId: 'foo',
-              siteId: 'bar',
-              screenshot_url:
-                'https://github.com/sanity-io/sanity-template-gatsby-blog/blob/dashboard/assets/studio-00.png?raw=true'
+              title: 'Netlify',
+              sites: [
+                <#<#deployments>#>
+                {
+                  buildHookId: '<#<provider.buildHookId>#>',
+                  name: '<#<name>#>',
+                  siteId: '<#<provider.siteId>#>'
+                },
+                <#</deployments>#>
+              ]
             }
-          },
-          {
-            group: 'Netlify sites',
-            title: 'Frontend',
-            value: 'https://test-sanity.netlify.com',
-            component: NetlifySitePreview,
-            options: {
-              buildHookId: 'foo',
-              siteId: 'bar',
-              screenshot_url:
-                'https://github.com/sanity-io/sanity-template-gatsby-blog/blob/dashboard/assets/preview-00.jpg?raw=true'
-            }
-          },
-          {
-            group: 'Code',
-            title: 'Repo',
-            value: 'https://github.com/<#<repository.owner>#>/<#<repository.name>#>'
           }
-        ]
+        ],
       }
     },
-    // {
-    //   name: 'netlify',
-    //   options: {
-    //     title: 'Netlify',
-    //     sites: [
-    //       <#<#deployments>#>
-    //       {
-    //         buildHookId: '<#<provider.buildHookId>#>',
-    //         name: '<#<name>#>',
-    //         siteId: '<#<provider.siteId>#>'
-    //       },
-    //       <#</deployments>#>
-    //     ]
-    //   }
-    // },
-    {name: 'project-users'},
+    {name: 'project-users', layout: {height: 'auto'}},
     {
       name: 'document-list',
       options: {title: 'Recently edited', order: '_updatedAt desc', limit: 10}
     },
-    // {name: 'document-list', options: {title: 'Recently created'}},
-    // {name: 'document-list'},
     {
       name: 'document-list',
       options: {title: 'Recent blog posts', order: '_createdAt desc', limit: 10, types: ['post']}
--- template/studio/plugins/dashboard-widget-project-info/src/index.js
+++
@@ -1,9 +0,0 @@
-import {withPropsStream} from 'react-props-stream'
-import ProjectInfoWidget from './ProjectInfoWidget'
-import {toPropsStream} from './props'
-
-export default {
-  name: 'dev/project-info',
-  component: withPropsStream(toPropsStream, ProjectInfoWidget),
-  layout: {width: 'medium'}
-}
