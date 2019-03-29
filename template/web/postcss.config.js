402a27f173ad5a26b00a14fcb632bcea59e510fc
Marius Lundgård <studio@mariuslundgard.com>
2019-03-29 16:43:19 +0100

    Update design details

--- template/web/postcss.config.js
+++ template/web/postcss.config.js
@@ -2,7 +2,17 @@ module.exports = () => ({
   plugins: [
     require('postcss-import'),
     require('postcss-preset-env')({
-      stage: 0
+      stage: 3,
+      features: {
+        'color-mod-function': {unresolved: 'warn'},
+        'nesting-rules': true,
+        'custom-media-queries': {
+          preserve: false
+        },
+        'custom-properties': {
+          preserve: false
+        }
+      }
     })
   ]
 })
--- template/web/src/components/author-list.js
+++ template/web/src/components/author-list.js
@@ -4,38 +4,36 @@ import {imageUrlFor} from '../lib/image-url'
 
 import styles from './author-list.module.css'
 
-function ucfirst (str) {
-  return `${str.substr(0, 1).toUpperCase()}${str.substr(1)}`
-}
-
 function AuthorList ({items, title}) {
   return (
     <div className={styles.root}>
       <h2 className={styles.headline}>{title}</h2>
       <ul className={styles.list}>
-        {items.map(({author, _key}) => (
-          <li key={_key} className={styles.listItem}>
-            <div>
-              <div className={styles.avatar}>
-                {author && author.image && author.image.asset && (
-                  <img
-                    src={imageUrlFor(buildImageObj(author.image))
-                      .width(100)
-                      .height(100)
-                      .fit('crop')
-                      .url()}
-                    alt=''
-                  />
-                )}
-              </div>
-            </div>
-            <div>
+        {items.map(({author, _key}) => {
+          console.log(author)
+          const authorName = author && author.name
+          return (
+            <li key={_key} className={styles.listItem}>
               <div>
-                <strong>{(author && author.name) || <em>Missing</em>}</strong>
+                <div className={styles.avatar}>
+                  {author && author.image && author.image.asset && (
+                    <img
+                      src={imageUrlFor(buildImageObj(author.image))
+                        .width(100)
+                        .height(100)
+                        .fit('crop')
+                        .url()}
+                      alt=''
+                    />
+                  )}
+                </div>
               </div>
-            </div>
-          </li>
-        ))}
+              <div>
+                <div>{authorName || <em>Missing name</em>}</div>
+              </div>
+            </li>
+          )
+        })}
       </ul>
     </div>
   )
