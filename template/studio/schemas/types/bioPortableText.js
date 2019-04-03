ab1db7cb6ea5ded9f7095bc84f297a07be40ab55
Knut Melvær <knut.melvaer@gmail.com>
2019-04-03 11:00:09 +0200

    remove link (it's there by default)

--- template/studio/schemas/types/bioPortableText.js
+++ template/studio/schemas/types/bioPortableText.js
@@ -13,20 +13,6 @@ export default {
           {title: 'Strong', value: 'strong'},
           {title: 'Emphasis', value: 'em'},
           {title: 'Code', value: 'code'}
-        ],
-        annotations: [
-          {
-            name: 'link',
-            type: 'object',
-            title: 'URL',
-            fields: [
-              {
-                title: 'URL',
-                name: 'href',
-                type: 'url'
-              }
-            ]
-          }
         ]
       }
     }
