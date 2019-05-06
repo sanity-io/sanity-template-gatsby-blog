382af5900420a449190fb9cf978e7dd6fe54a5f0
Knut Melvær <knut.melvaer@gmail.com>
2019-05-06 13:27:06 +0200

    Add Figure/mainImage to serializer

--- template/web/src/components/portableText.js
+++ template/web/src/components/portableText.js
@@ -1,12 +1,7 @@
+import React from 'react'
 import clientConfig from '../../client-config'
 import BasePortableText from '@sanity/block-content-to-react'
-import React from 'react'
-
-const serializers = {
-  types: {
-    authorReference: ({node}) => <span>{node.author.name}</span>
-  }
-}
+import serializers from './serializers'
 
 const PortableText = ({blocks}) => (
   <BasePortableText blocks={blocks} serializers={serializers} {...clientConfig.sanity} />
