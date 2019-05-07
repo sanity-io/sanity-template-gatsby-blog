2c081947999829f6ff1e9f989b2bd6bd0a40fd58
Knut Melvær <knut.melvaer@gmail.com>
2019-05-07 07:03:18 +0200

    Fix env config

--- template/web/client-config.js
+++ template/web/client-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   sanity: {
-    projectId: process.env.GATSBY_SANITY_PROJECT_ID,
-    dataset: process.env.GATSBY_SANITY_DATASET
+    projectId: process.env.GATSBY_SANITY_PROJECT_ID || '<#< sanity.projectId >#>',
+    dataset: process.env.GATSBY_SANITY_DATASET || '<#< sanity.dataset >#>'
   }
 }
