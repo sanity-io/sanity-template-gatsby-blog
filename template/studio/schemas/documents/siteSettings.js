b9cbb7241844fc2d1f7316b3c3eb381e08625e47
Knut Melvær <knut@sanity.io>
2019-05-28 22:36:28 +0200

    Add actions to settings

--- template/studio/schemas/documents/siteSettings.js
+++ template/studio/schemas/documents/siteSettings.js
@@ -2,6 +2,7 @@ export default {
   name: 'siteSettings',
   type: 'document',
   title: 'Site Settings',
+  __experimental_actions: [/*'create',*/ 'update', /*'delete',*/ 'publish'],
   fields: [
     {
       name: 'title',
