87dffa34fb5459c97242b17166a2ffb002ff9849
Knut Melvær <knut@sanity.io>
2019-05-30 21:36:52 +0200

    Use webp

--- template/web/src/components/blog-post-preview.js
+++ template/web/src/components/blog-post-preview.js
@@ -20,6 +20,7 @@ function BlogPostPreview (props) {
             src={imageUrlFor(buildImageObj(props.mainImage))
               .width(600)
               .height(Math.floor((9 / 16) * 600))
+              .auto('format')
               .url()}
             alt={props.mainImage.alt}
           />
