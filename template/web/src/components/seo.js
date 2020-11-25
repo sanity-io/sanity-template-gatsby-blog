db1d17984f1f17cf9bda863869afae5fae527139
Corey Ward <corey.atx@gmail.com>
2020-11-25 13:51:20 -0600

    wip

--- template/web/src/components/seo.js
+++ template/web/src/components/seo.js
@@ -1,80 +1,72 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Helmet from "react-helmet";
-import { StaticQuery, graphql } from "gatsby";
+import { graphql, useStaticQuery } from "gatsby";
 import { imageUrlFor } from "../lib/image-url";
 import { buildImageObj } from "../lib/helpers";
 
 function SEO({ description, lang, meta, keywords, title, image }) {
-  return (
-    <StaticQuery
-      query={detailsQuery}
-      render={(data) => {
-        const metaDescription =
-          description || (data.site && data.site.description) || "";
-        const siteTitle = (data.site && data.site.title) || "";
-        const siteAuthor =
-          (data.site && data.site.author && data.site.author.name) || "";
-        const metaImage =
-          image && image.asset
-            ? imageUrlFor(buildImageObj(image)).width(1200).url()
-            : "";
+  const { site } = useStaticQuery(detailsQuery) || {};
 
-        return (
-          <Helmet
-            htmlAttributes={{ lang }}
-            title={title}
-            titleTemplate={title === siteTitle ? "%s" : `%s | ${siteTitle}`}
-            meta={[
-              {
-                name: "description",
-                content: metaDescription,
-              },
-              {
-                property: "og:title",
-                content: title,
-              },
-              {
-                property: "og:description",
-                content: metaDescription,
-              },
-              {
-                property: "og:type",
-                content: "website",
-              },
-              {
-                property: "og:image",
-                content: metaImage,
-              },
-              {
-                name: "twitter:card",
-                content: "summary",
-              },
-              {
-                name: "twitter:creator",
-                content: siteAuthor,
-              },
-              {
-                name: "twitter:title",
-                content: title,
-              },
-              {
-                name: "twitter:description",
-                content: metaDescription,
-              },
-            ]
-              .concat(
-                keywords && keywords.length > 0
-                  ? {
-                      name: "keywords",
-                      content: keywords.join(", "),
-                    }
-                  : []
-              )
-              .concat(meta)}
-          />
-        );
-      }}
+  const metaDescription = description || site.description || "";
+  const siteTitle = site.title || "";
+  const siteAuthor = site.author?.name || "";
+  const metaImage = image?.asset
+    ? imageUrlFor(buildImageObj(image)).width(1200).url()
+    : "";
+
+  return (
+    <Helmet
+      htmlAttributes={{ lang }}
+      title={title}
+      titleTemplate={title === siteTitle ? "%s" : `%s | ${siteTitle}`}
+      meta={[
+        {
+          name: "description",
+          content: metaDescription,
+        },
+        {
+          property: "og:title",
+          content: title,
+        },
+        {
+          property: "og:description",
+          content: metaDescription,
+        },
+        {
+          property: "og:type",
+          content: "website",
+        },
+        {
+          property: "og:image",
+          content: metaImage,
+        },
+        {
+          name: "twitter:card",
+          content: "summary",
+        },
+        {
+          name: "twitter:creator",
+          content: siteAuthor,
+        },
+        {
+          name: "twitter:title",
+          content: title,
+        },
+        {
+          name: "twitter:description",
+          content: metaDescription,
+        },
+      ]
+        .concat(
+          keywords && keywords.length > 0
+            ? {
+                name: "keywords",
+                content: keywords.join(", "),
+              }
+            : []
+        )
+        .concat(meta)}
     />
   );
 }
--- template/web/src/containers/layout.js
+++ template/web/src/containers/layout.js
@@ -1,5 +1,5 @@
-import { StaticQuery, graphql } from "gatsby";
 import React, { useState } from "react";
+import { graphql, useStaticQuery } from "gatsby";
 import Layout from "../components/layout";
 
 const query = graphql`
@@ -12,31 +12,29 @@ const query = graphql`
 
 function LayoutContainer(props) {
   const [showNav, setShowNav] = useState(false);
+
   function handleShowNav() {
     setShowNav(true);
   }
+
   function handleHideNav() {
     setShowNav(false);
   }
+
+  const data = useStaticQuery(query);
+  if (!data.site) {
+    throw new Error(
+      'Missing "Site settings". Open the Studio at http://localhost:3333 and some content in "Site settings"'
+    );
+  }
+
   return (
-    <StaticQuery
-      query={query}
-      render={(data) => {
-        if (!data.site) {
-          throw new Error(
-            'Missing "Site settings". Open the Studio at http://localhost:3333 and some content in "Site settings"'
-          );
-        }
-        return (
-          <Layout
-            {...props}
-            showNav={showNav}
-            siteTitle={data.site.title}
-            onHideNav={handleHideNav}
-            onShowNav={handleShowNav}
-          />
-        );
-      }}
+    <Layout
+      {...props}
+      showNav={showNav}
+      siteTitle={data.site.title}
+      onHideNav={handleHideNav}
+      onShowNav={handleShowNav}
     />
   );
 }
