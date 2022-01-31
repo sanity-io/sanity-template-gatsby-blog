ac38daea1d4ce253495ac5fa695dd239d5ddfa03
Henrique Doro <opensource@hdoro.dev>
2022-01-31 15:55:47 -0300

    feat: migrate to @portabletext/react

--- template/web/src/components/portableText.js
+++ template/web/src/components/portableText.js
@@ -1,12 +1,21 @@
+import { PortableText as BasePortableText } from "@portabletext/react";
 import React from "react";
 import clientConfig from "../../client-config";
-import BasePortableText from "@sanity/block-content-to-react";
-import serializers from "./serializers";
+import { Figure } from "./Figure";
+
+const components = {
+  types: {
+    /* eslint-disable-next-line react/display-name */
+    authorReference: ({ node }) => <span>{node.author.name}</span>,
+    mainImage: Figure,
+  },
+};
+
 
 const PortableText = ({ blocks }) => (
   <BasePortableText
-    blocks={blocks}
-    serializers={serializers}
+    value={blocks}
+    components={components}
     {...clientConfig.sanity}
   />
 );
